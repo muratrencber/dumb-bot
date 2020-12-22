ef07907616e0e10b0b4bd3c2f86e0a1e1b4db2ad
Murat Rençber <73824069+ituitis20-rencber19@users.noreply.github.com>
2020-12-22 16:55:13 +0300

    I hate bottle

--- requester.py
+++ requester.py
@@ -44,8 +44,9 @@ def post():
 
     redirect("/istek?fromsubmit=false")
 
+if __name__ == '__main__':
+    # Get required port, default to 5000.
+    port = os.environ.get('PORT', 5000)
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+    # Run the app.
+    run(host='0.0.0.0', port=port)
